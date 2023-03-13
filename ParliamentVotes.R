2de8533f90e9c3f78686aaddc68eea3a9f454f1d
dgrant59 <danielgrant422@gmail.com>
2023-03-13 02:14:40 -0400

    Cleaned up R code. Still to fix name encoding issue, but code can be used by anyone now to reproduce

--- ParliamentVotes.R
+++ ParliamentVotes.R
@@ -2,41 +2,34 @@ library(sf)
 library(dplyr)
 library(fuzzyjoin)
 library(stringr)
-ED <- st_read(dsn = "./ElectoralDistrictMaps/ElectoralDistricts_Simplified.shp",options = "ENCODING=WINDOWS-1252") 
-ED <- ED %>% arrange(FEDENAME)
-ED <- ED %>% mutate(FEDENAME = gsub("--","—",FEDENAME),
-                    FEDFNAME = gsub("--","—",FEDFNAME),
-                    FEDNAME = gsub("--","—",FEDNAME))
+library(XML)
+library(plyr)
+library(RCurl)
+
+#Take vote xml data and use this to create a csv file for each vote
+
+nvotes <- 261 #number of votes as of creating this, correct this to update it
+#add for(j in 1:nsession) if parliament is into 2nd session
+
+for(i in 1:nvotes){
+  vote_xml_url <- paste0("https://www.ourcommons.ca/Members/en/votes/44/1/",i,"/xml")
+  download_URL <- getURL(vote_xml_url)
+  temp_raw_xml <- xmlParse(download_URL)
+  tempvotedata <- xmlToDataFrame(temp_raw_xml)
+  # write.csv(tempvotedata,paste0("./VoteData/vote_",i,"_data.csv"))
+}
 
 
-
-members <- read.csv("./members.csv")
-members <- members %>% arrange(Constituency)
-members <- members %>% mutate(Constituency = gsub("--","—",Constituency))
-
-
-member_ED <- stringdist_join(ED,members,by=c("FEDENAME"="Constituency"),
-                      mode="left",method="jw",max_dist=0.02,distance_col="dist") %>%
-  group_by(FEDENAME) %>% slice_min(order_by=dist,n=1)
+#Get metadata on each vote (name of bill, subject, vote results, time of vote, etc.)
+vote_xml_url <- "https://www.ourcommons.ca/Members/en/votes/xml"
+download_URL <- getURL(vote_xml_url)
+temp_raw_xml <- xmlParse(download_URL,ignoreBlanks = F)
+data <- xmlToDataFrame(temp_raw_xml)
+write.csv(data,"./VoteData/MetaDataVotes.csv")
 
 
-no_member_ED <- ED[which(ED$FEDUID%in%setdiff(ED$FEDUID,member_ED$FEDUID)),]
-
-ok <- bind_rows(data.frame(no_member_ED),data.frame(member_ED))
-ok$geometry <- c(no_member_ED$geometry, member_ED$geometry)
-ok <- st_as_sf(ok)
-ok <- ok %>% mutate(Name = paste0(Honorific.Title,
-                                  First.Name,
-                                  " ",
-                                  Last.Name)) %>% 
-  relocate(Name,.after=PRUID) %>% select(-c(Honorific.Title,
-                                                            First.Name,
-                                                            Last.Name))
-                                 #                        PersonOfficialFirstName,
-                                 #                        " ",
-                                 #                        PersonOfficialLastName))
-View(ok %>% select(FEDENAME,Constituency,dist))
-
+#Getting the constituencies of all past (voting) HoC members. Speaker does not vote
+#and must be added manually.
 
 vote1 <- read.csv("./VoteData/vote_1_data.csv")
 
@@ -47,79 +40,43 @@ for(i in 2:nvotes){
   votetemp <- read.csv(paste0("./VoteData/vote_",i,"_data.csv"))
   membertemp <- votetemp %>%
     select(PersonId,ConstituencyName)
-  
-  members_constituency <- full_join(members_constituency,membertemp,by=c("PersonId","ConstituencyName"))
-  
-  
+  members_constituency <- full_join(members_constituency,
+                                    membertemp,
+                                    by=c("PersonId","ConstituencyName"))
 }
-members_constituency <- rbind(members_constituency,data.frame(PersonId=25452,
-                                                              ConstituencyName = "Nipissing—Timiskaming"))
+#add the speaker
+members_constituency <- rbind(members_constituency,
+                              data.frame(PersonId=25452,
+                                         ConstituencyName = "Nipissing—Timiskaming"))
+write.csv(members_constituency,"members_constituency.csv") #can write now, but will add 
+#to it at the bottom of this file
+
+
+
 #members_constituency <- members_constituency[-184,]
-write.csv(members_constituency,"members_constituency.csv")
 
+#Get parliament member tenure data
 
-ED2 <- stringdist_join(ED,members_constituency,by=c("FEDENAME"="ConstituencyName"),
-                             mode="full",method="jw",max_dist=0.99,distance_col="dist") %>%
-  group_by(FEDENAME) %>% slice_min(order_by=dist,n=1) %>% ungroup()
-ED2 <- ED2 %>% select(-c(FEDNAME,FEDENAME,FEDFNAME))
-ED2 <- ED2[-c(184),]
-st_write(ED2,"./ElectoralDistrictMaps/EDPersonId.shp",layer_options = "ENCODING=UTF-8", delete_layer = TRUE)
-
-library(XML)
-library(plyr)
-library(RCurl)
-# nvotes <- 261
-# for(j in 1:nsession)
-# for(i in 1:nvotes){
-# 
-#   vote_xml_url <- paste0("https://www.ourcommons.ca/Members/en/votes/44/1/",i,"/xml")
-#   download_URL <- getURL(vote_xml_url)
-#   temp_raw_xml <- xmlParse(download_URL)
-#   temp_list <- xmlToList(temp_raw_xml) ###is to convert xml doc into List
-#   data <- ldply(temp_list, data.frame)
-# 
-#   write.csv(data,paste0("./VoteData/vote_",i,"_data.csv"))
-# }
-
-#Change above code to reflect xmlToDataFrame?
-# 
-# # vote_xml_url <- "https://www.ourcommons.ca/Members/en/votes/xml"
-#    download_URL <- getURL(vote_xml_url)
-#    temp_raw_xml <- xmlParse(download_URL,ignoreBlanks = F)
-#    data <- xmlToDataFrame(temp_raw_xml) ###is to convert xml doc into List
-#    write.csv(data,"./VoteData/MetaDataVotes.csv")
-
-#Get parliament member data
-
-vote1 <- read.csv("./VoteData/vote_1_data.csv")
-
-members_tenure <- vote1 %>% select(PersonId)
-
-for(i in 2:nvotes){
+for(i in 1:nrow(members_constituency)){
   print(i)
-  votetemp <- read.csv(paste0("./VoteData/vote_",i,"_data.csv"))
-  membertemp <- votetemp %>%
-    select(PersonId)
-
-  members_tenure <- full_join(members_tenure,membertemp,by="PersonId")
+  member_csv_url <- paste0("https://www.ourcommons.ca/members/en/",members_constituency$PersonId[i],"/csv")
+  download.file(member_csv_url,paste0("./Members/",members_constituency$PersonId[i],".csv"))
 }
 
-# for(i in 1:nrow(members_tenure)){
-#   print(i)
-#   member_csv_url <- paste0("https://www.ourcommons.ca/members/en/",members_tenure$PersonId[i],"/csv")
-#   download.file(member_csv_url,paste0())
-# }
+members_tenure <- members_constituency
+members_tenure$StartDate <- NA
+members_tenure$EndDate <- NA
 
-for(i in 1:nrow(members_tenure)){
-  temp_members <- read.csv(paste0("./Members/",members_tenure$PersonId[i],".csv"),skip=1,header = T)
+for(i in 1:nrow(members_constituency)){
+  temp_members <- read.csv(paste0("./Members/",members_constituency$PersonId[i],".csv"),skip=1,header = T)
   members_tenure$StartDate[i] <- temp_members[1,7]
   members_tenure$EndDate[i] <- temp_members[1,8]
 }
 
 #add the speaker of the house, Anthony Rota (doesn't vote)
-members_tenure <- rbind(members_tenure, data.frame(PersonId=25452,
-                                                   StartDate="2021-09-20 12:00:00 AM",
-                                                   EndDate=NA))
+# members_tenure <- rbind(members_tenure, data.frame(PersonId=25452,
+#                                                    StartDate="2021-09-20 12:00:00 AM",
+#                                                    EndDate=NA))
 write.csv(members_tenure,"members_tenure.csv")
 
 members_caucus <- members_tenure
@@ -136,6 +93,7 @@ for(i in 1:nrow(members_caucus)){
     members_caucus[i,]$Caucus <- temp_members[1,6]
     }
 }
+#Add Alain Rayes, only member to change caucuses mid session
 members_caucus <- rbind(members_caucus,data.frame(PersonId=88600,
                                                   StartDate="2021-09-20 12:00:00 AM",
                                                   EndDate="2022-09-13 1:05:00 PM",
@@ -157,6 +115,14 @@ write.csv(members_caucus,"members_caucus.csv")
 #   }
 #   
 # }
+
+# combine vote data to get a large table of votes. 
+# Need to be careful here because people who did not vote are omitted from the voting
+# data. Did people who didn't vote not vote because they weren't present, they chose not to,
+# or because they were not elected yet (Charles Sousa prior to Jan 2023) or had resigned prior? 
+# Need to be manually add such members based on the members of parliament roll from 
+# defined by members_caucus dates
+
 library(data.table)
 vote1 <- fread("./VoteData/vote_1_data.csv",drop=1)
 vote_data <- vote1 %>%
@@ -200,23 +166,6 @@ for(j in 1:nrow(vote_i_members)){
 }  
 
 
-# data.frame(.id = NULL,
-#            DecisionEventDateTime = NULL,
-#            ConsituencyName = NULL,
-#            VoteValueName = NULL,
-#            PersonOfficialFirstName = NULL,
-#            PersonOfficialLastName = NULL,
-#            ConstituencyProvinceTerritoryName = NULL,
-#            CaucusShortName = NULL,
-#            IsVoteYea = NULL,
-#            IsVoteNay = NULL,
-#            IsVotePaired = NULL,
-#            DecisionResultName = NULL,
-#            PersonId = NULL,
-#            VoteId = NULL)
-
-
-  
 for(i in 2:nvotes){
   print(i)
   votetemp <- fread(paste0("./VoteData/vote_",i,"_data.csv"),drop=1)
@@ -241,7 +190,7 @@ for(i in 2:nvotes){
   vote_i_members <- vote_i_members[vote_i_members$PersonId %in% setdiff(vote_i_members$PersonId,votetemp$PersonId),]
   
   
-  
+ #if there are members who were employed but did not vote, add them as "Did not vote" 
   if(nrow(vote_i_members)>0){
     for(j in 1:nrow(vote_i_members)){
       
@@ -266,50 +215,13 @@ for(i in 2:nvotes){
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-# get affiliation for each member
-# members$
-# for(i in 1:nrow(members)){
-#   temp <- read.csv(paste0("./Members/",members$PersonId[i],".csv"),skip=1,header=T)
-# }
-
-
-
-vote1 <- read.csv("./VoteData/vote_1_data.csv")
-vote_data <- vote1 %>%
-  mutate(VoteId = paste0(ParliamentNumber,
-                         SessionNumber,
-                         DecisionDivisionNumber)) %>%
-  select(-c(ParliamentNumber,
-            SessionNumber,
-            DecisionDivisionNumber,X))
-
-for(i in 2:nvotes){
-  print(i)
-  votetemp <- read.csv(paste0("./VoteData/vote_",i,"_data.csv"))
-  votetemp <- votetemp %>%
-    mutate(VoteId = paste0(ParliamentNumber,
-                           SessionNumber,
-                           DecisionDivisionNumber)) %>%
-    select(-c(ParliamentNumber,
-              SessionNumber,
-              DecisionDivisionNumber,X))
-  vote_data <- rbind(vote_data, votetemp)
-}
-# vote_data <- vote_data %>% pivot_wider(names_from = PersonId,values_from = VoteValueName)
-
 write.csv(vote_data,"vote_data.csv")
 
 
+# Get pictures of members from parliament website
+# Pictures have the form LastNameFirstName_ShortCaucus
+# but there are a few members with name or party changes that affect
+# their url. need to download those manually. 
 
 Member_pic <- vote_data %>% select(PersonOfficialLastName,
                      PersonOfficialFirstName,
@@ -330,40 +242,92 @@ Member_pic <- vote_data %>% select(PersonOfficialLastName,
   select(Pic_URL,PersonId) %>%
   distinct(Pic_URL,PersonId)
 
-Member_pic$Pic_URL[246] <- "RempelMichelle_CPC"
+Member_pic<-Member_pic[-c(339),] # Remove alain rayes ind, since his photo was taken as a CPC
+#Some members that need edits to their urls. 
 Member_pic$Pic_URL[12] <- "ArseneaultRené_Lib"
 Member_pic$Pic_URL[65] <- "ChongMichaelD_CPC"
 Member_pic$Pic_URL[107] <- "FindlayKerryLynne_CPC"
 Member_pic$Pic_URL[119] <- "GaronJeanDenis_BQ"
-
-# 121"GaudreauMarieHélène_BQ"
-# 142 "HussenAhmedD_Lib"
-# 168"LalondeMarieFrance_Lib"
-# 190 "MacDonaldHeath_CPC"
-# 192 "MacKenzieDavid_CPC"
-# 204 "McGuintyDavidJ._Lib"
-# 208 "McLeodMichaelV_Lib"
-# 220 "MorrisseyRobertJ._Lib"
-# 234 "PaulHusPierre_CPC"
-# 256 "SajjanHarjit_Lib"
-# 266 "SgroJudyA_Lib"
-# 274 "SinclairDesgagnéNathalie_BQ"
-# 281 "SteMarieGabriel_BQ"
-# 296 "TurnbullRyan_CPC"
-# 328 "BarsalouDuvalXavier_BQ"
-# 333 "SavardTremblaySimonPierre_BQ"
-# 339 "RayesAlain_CPC"
+Member_pic$Pic_URL[121] <-"GaudreauMarieHélène_BQ"
+Member_pic$Pic_URL[142] <-"HussenAhmedD_Lib"
+Member_pic$Pic_URL[168] <-"LalondeMarieFrance_Lib"
+Member_pic$Pic_URL[190] <- "MacDonaldHeath_CPC"
 Member_pic$Pic_URL[191] <-"MacGregorAllistair_NDP"
-for(i in 340:nrow(Member_pic)){
+Member_pic$Pic_URL[192] <- "MacKenzieDavid_CPC"
+Member_pic$Pic_URL[204] <- "McGuintyDavidJ._Lib"
+Member_pic$Pic_URL[208] <- "McLeodMichaelV_Lib"
+Member_pic$Pic_URL[220] <- "MorrisseyRobertJ._Lib"
+Member_pic$Pic_URL[234] <- "PaulHusPierre_CPC"
+Member_pic$Pic_URL[246] <- "RempelMichelle_CPC"
+Member_pic$Pic_URL[256] <- "SajjanHarjit_Lib"
+Member_pic$Pic_URL[266] <- "SgroJudyA_Lib"
+Member_pic$Pic_URL[274] <- "SinclairDesgagnéNathalie_BQ"
+Member_pic$Pic_URL[281] <- "SteMarieGabriel_BQ"
+Member_pic$Pic_URL[296] <- "TurnbullRyan_CPC"
+Member_pic$Pic_URL[328] <- "BarsalouDuvalXavier_BQ"
+Member_pic$Pic_URL[333] <- "SavardTremblaySimonPierre_BQ"
+Member_pic$Pic_URL[339] <- "RayesAlain_CPC"
+
+
+#Something to do with encoding is resulting in errors with names with special
+# (accented) letters.
+for(i in 1:nrow(Member_pic)){
     print(i)
-    member_pic_url <- paste0("https://www.ourcommons.ca/Content/Parliamentarians/Images/OfficialMPPhotos/44/",Member_pic$Pic_URL[i],".jpg")
-    download.file(member_pic_url,paste0("./Members/Photos/",i,".jpg"),mode = 'wb')
+    print(Member_pic$PersonId[i])
+    member_pic_url <- paste0("https://www.ourcommons.ca/Content/Parliamentarians/Images/OfficialMPPhotos/44/",
+                             Member_pic$Pic_URL[i],
+                             ".jpg")
+    download.file(member_pic_url,paste0("./Members/Photos/",Member_pic$PersonId[i],".jpg"),mode = 'wb')
 }
 
-file.rename(paste0("./Members/Photos/",list.files(path="./Members/Photos/",pattern="*.jpg")), paste0("./Members/Photos/",Member_pic$PersonId,".jpg"))
-library(base64enc)
-base64encode()
-
-
-members_constituency$Pic_URL <- paste0("https://raw.githubusercontent.com/dgrant59/ParliamentVotes/main/Members/Photos/",members_constituency$PersonId,".jpg")
+#I hosted the members on my github so scraping photos isn't necessary for others
+members_constituency$Pic_URL <- paste0("https://raw.githubusercontent.com/dgrant59/ParliamentVotes/main/Members/Photos/",
+                                       members_constituency$PersonId,
+                                       ".jpg")
 write.csv(members_constituency,"members_constituency.csv")
+
+
+
+
+# Code for potential geographical map
+
+# ED <- st_read(dsn = "./ElectoralDistrictMaps/ElectoralDistricts_Simplified.shp",options = "ENCODING=WINDOWS-1252") 
+# ED <- ED %>% arrange(FEDENAME)
+# ED <- ED %>% mutate(FEDENAME = gsub("--","—",FEDENAME),
+#                     FEDFNAME = gsub("--","—",FEDFNAME),
+#                     FEDNAME = gsub("--","—",FEDNAME))
+# 
+# 
+# 
+# members <- read.csv("./members.csv")
+# members <- members %>% arrange(Constituency)
+# members <- members %>% mutate(Constituency = gsub("--","—",Constituency))
+# 
+# 
+# member_ED <- stringdist_join(ED,members,by=c("FEDENAME"="Constituency"),
+#                       mode="left",method="jw",max_dist=0.02,distance_col="dist") %>%
+#   group_by(FEDENAME) %>% slice_min(order_by=dist,n=1)
+# 
+# 
+# no_member_ED <- ED[which(ED$FEDUID%in%setdiff(ED$FEDUID,member_ED$FEDUID)),]
+# 
+# ok <- bind_rows(data.frame(no_member_ED),data.frame(member_ED))
+# ok$geometry <- c(no_member_ED$geometry, member_ED$geometry)
+# ok <- st_as_sf(ok)
+# ok <- ok %>% mutate(Name = paste0(Honorific.Title,
+#                                   First.Name,
+#                                   " ",
+#                                   Last.Name)) %>% 
+#   relocate(Name,.after=PRUID) %>% select(-c(Honorific.Title,
+#                                                             First.Name,
+#                                                             Last.Name))
+#                                  #                        PersonOfficialFirstName,
+#                                  #                        " ",
+#                                  #                        PersonOfficialLastName))
+# View(ok %>% select(FEDENAME,Constituency,dist))
+# ED2 <- stringdist_join(ED,members_constituency,by=c("FEDENAME"="ConstituencyName"),
+#                        mode="full",method="jw",max_dist=0.99,distance_col="dist") %>%
+#   group_by(FEDENAME) %>% slice_min(order_by=dist,n=1) %>% ungroup()
+# ED2 <- ED2 %>% select(-c(FEDNAME,FEDENAME,FEDFNAME))
+# ED2 <- ED2[-c(184),]
+# st_write(ED2,"./ElectoralDistrictMaps/EDPersonId.shp",layer_options = "ENCODING=UTF-8", delete_layer = TRUE)
